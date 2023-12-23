e0f78b2e51c2c7023aec788a1ceeb2a62303bb57
Ngnawen Samuel <ngnawens@gmail.com>
2023-12-23 07:18:30 -0500

    build fail and trigger error

--- src/test/java/com/belrose/springbootjenkins/SpringBootJenkinsApplicationTests.java
+++ src/test/java/com/belrose/springbootjenkins/SpringBootJenkinsApplicationTests.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SpringBootJenkinsApplicationTests {
 
 	@Test
-	void contextLoads() {
+	 contextLoads() {
 		log.info("Test case executing...");
 		log.info("Test case executing... email sent");
 		assertEquals(true,true);
